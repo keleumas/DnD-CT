9145992bedd78b8599ee519a7b09eb15c66ea52c
keleumas <92540827+keleumas@users.noreply.github.com>
2022-03-11 20:37:38 +0100

    Delete Combat_Tracker directory

--- Combat_Tracker/creature.cpp
+++
@@ -1,9 +0,0 @@
-#include "creature.h"
-
-creature::creature(QObject *parent, QString name, int hp, int ac, int ini) : QObject(parent)
-{
-    this->name=name;
-    this->hp=hp;
-    this->ac=ac;
-    this->ini=ini;
-}
